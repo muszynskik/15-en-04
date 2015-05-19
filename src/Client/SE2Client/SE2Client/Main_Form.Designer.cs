bf1963f2e45b753edb504c1625f35d758c418c6d
Karol Muszynski <karol.muszynski@vp.pl>
2015-05-19 13:40:17 +0200

    Added new client which will be capable of tracking route of the solution.

---
+++ src/Client/SE2Client/SE2Client/Main_Form.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace SE2Client
+{
+	partial class Main_Form
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose( bool disposing )
+		{
+			if ( disposing && (components != null) )
+			{
+				components.Dispose( );
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent( )
+		{
+            this.IP_TextBox = new System.Windows.Forms.TextBox();
+            this.Connect_Button = new System.Windows.Forms.Button();
+            this.SendRequest_Button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // IP_TextBox
+            // 
+            this.IP_TextBox.Location = new System.Drawing.Point(12, 12);
+            this.IP_TextBox.Name = "IP_TextBox";
+            this.IP_TextBox.Size = new System.Drawing.Size(135, 20);
+            this.IP_TextBox.TabIndex = 0;
+            this.IP_TextBox.Text = "192.168.143.13";
+            // 
+            // Connect_Button
+            // 
+            this.Connect_Button.Location = new System.Drawing.Point(153, 10);
+            this.Connect_Button.Name = "Connect_Button";
+            this.Connect_Button.Size = new System.Drawing.Size(75, 23);
+            this.Connect_Button.TabIndex = 1;
+            this.Connect_Button.Text = "Connect";
+            this.Connect_Button.UseVisualStyleBackColor = true;
+            this.Connect_Button.Click += new System.EventHandler(this.Connect_Button_Click);
+            // 
+            // SendRequest_Button
+            // 
+            this.SendRequest_Button.Location = new System.Drawing.Point(12, 226);
+            this.SendRequest_Button.Name = "SendRequest_Button";
+            this.SendRequest_Button.Size = new System.Drawing.Size(216, 23);
+            this.SendRequest_Button.TabIndex = 2;
+            this.SendRequest_Button.Text = "Send request";
+            this.SendRequest_Button.UseVisualStyleBackColor = true;
+            this.SendRequest_Button.Click += new System.EventHandler(this.SendRequest_Button_Click);
+            // 
+            // Main_Form
+            // 
+            this.AcceptButton = this.Connect_Button;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(239, 261);
+            this.Controls.Add(this.SendRequest_Button);
+            this.Controls.Add(this.Connect_Button);
+            this.Controls.Add(this.IP_TextBox);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Main_Form";
+            this.Text = "Client";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TextBox IP_TextBox;
+		private System.Windows.Forms.Button Connect_Button;
+		private System.Windows.Forms.Button SendRequest_Button;
+	}
+}
+
---
+++ src/Client/SE2Client/SE2Client/Main_Form.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Xml;
+using System.Windows.Forms.Design;
+
+
+
+namespace SE2Client
+{
+	public partial class Main_Form : Form
+	{
+		byte[] bytes = new byte[1024];
+		Socket senderSock;
+		
+		public Main_Form( )
+		{
+			InitializeComponent( );
+		}
+
+		private void Connect_Button_Click( object sender, EventArgs e )
+		{
+			try
+			{
+				/// Create one SocketPermission for socket access restrictions 
+				SocketPermission permission = new SocketPermission(
+					NetworkAccess.Connect,    /// Connection permission 
+					TransportType.Tcp,        /// Defines transport types 
+					"",                       /// Gets the IP addresses 
+					SocketPermission.AllPorts /// All ports 
+					);
+
+				/// Ensures the code to have permission to access a Socket 
+				permission.Demand( );
+
+				/// Resolves a host name to an IPHostEntry instance            
+				IPHostEntry ipHost = Dns.GetHostEntry( IP_TextBox.Text );
+
+				/// Gets first IP address associated with a localhost 
+				IPAddress ipAddr = ipHost.AddressList[0];
+
+				/// Creates a network endpoint 
+				IPEndPoint ipEndPoint = new IPEndPoint(ipAddr, 6666);
+
+				/// Create one Socket object to setup Tcp connection 
+				senderSock = new Socket(
+					ipAddr.AddressFamily,/// Specifies the addressing scheme 
+					SocketType.Stream,   /// The type of socket  
+					ProtocolType.Tcp     /// Specifies the protocols  
+					);
+
+				senderSock.NoDelay = false;   /// Using the Nagle algorithm 
+
+				/// Establishes a connection to a remote host 
+				senderSock.Connect(ipEndPoint);
+				IP_TextBox.Text = "Socket connected to " + senderSock.RemoteEndPoint.ToString( );
+
+				///Connect_Button.IsEnabled = false;
+				//button2.Enabled = true;
+			}
+			catch ( Exception ex )
+			{
+				MessageBox.Show( ex.ToString( ) );
+			}
+		}
+		
+		public string GetXMLAsString( XmlDocument myxml )
+		{
+			return myxml.OuterXml;
+		}
+
+
+		/// <summary>
+		/// Receives the data from the server in bytes form.
+		/// </summary>
+		private void _ReceiveData( )
+		{
+			try
+			{
+				/// Receives data from a bound Socket. 
+				int bytesRec = senderSock.Receive(bytes);
+
+				/// Converts byte array to string 
+				/// String theMessageToReceive = Encoding.Unicode.GetString(bytes, 0, bytesRec);
+				String theMessageToReceive = Encoding.ASCII.GetString(bytes);
+
+				/// Continues to read the data till data isn't available 
+				while ( senderSock.Available > 0 )
+				{
+					bytesRec = senderSock.Receive(bytes);
+					theMessageToReceive += Encoding.Unicode.GetString(bytes, 0, bytesRec);
+				}
+
+				IP_TextBox.Text = "The server reply: " + theMessageToReceive;
+
+			}
+			catch ( Exception ex )
+			{
+				MessageBox.Show( ex.ToString( ) );
+			}
+		} /// end of private void _ReceiveData( )
+
+
+		public void msg( string mesg )
+		{
+
+			IP_TextBox.Text = IP_TextBox.Text + Environment.NewLine + " >> " + mesg;
+
+		}
+
+		private void SendRequest_Button_Click( object sender, EventArgs e )
+		{
+			try
+			{
+				/// Sending message 
+				XmlDocument registerMsg = new XmlDocument( );
+				registerMsg.Load("xml/registerMsg.xml");
+				XmlNode node = registerMsg.SelectSingleNode("//Type");
+				node.InnerText = "TaskManager";
+				/// TESTING
+				/// registerMsg.Save("blabla.xml");
+				string theMessageToSend = GetXMLAsString(registerMsg);
+				byte[] msg = Encoding.Unicode.GetBytes(theMessageToSend);
+
+				/// Sends data to a connected Socket. 
+				int bytesSend = senderSock.Send(msg);
+
+				_ReceiveData( );
+				///bytes [i]
+
+				PictureBox point = new PictureBox( );
+				ToolTip toolTip = new ToolTip( );
+                string s = "";
+				/// show the path
+				int x = 0;
+				for ( int i=0; i<1024; i++ )
+				{
+					if ( bytes[i].ToString( ) != "!" )
+						x++;
+                }
+                for (int i = 0; i <x; i++)
+                {
+                    s +=bytes[i].ToString();
+                    MessageBox.Show( ( (char)Convert.ToInt32( bytes[i] ) ).ToString( ) );
+					point = new PictureBox( );
+					point.Size = new System.Drawing.Size(30, 37);
+					point.BackgroundImage = global::SE2Client.Properties.Resources.unnamed1;
+					(( Bitmap )(point.BackgroundImage)).MakeTransparent(Color.White);
+					point.BackgroundImageLayout = ImageLayout.Zoom;
+					point.Location = new Point( 20*(i+1), 40*(i+1) );
+					toolTip = new ToolTip( );
+                    toolTip.SetToolTip(point, "asdfsadf");
+
+					this.Controls.Add( point );
+
+					// the arrow: global::SE2Client.Properties.Resources.arrow_27;
+				}
+                MessageBox.Show(s);
+			}
+			catch ( Exception ex )
+			{
+				MessageBox.Show(ex.ToString( ));
+			}
+
+		}
+	}
+}
